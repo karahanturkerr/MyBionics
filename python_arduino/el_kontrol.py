2849988fc83a1a3c2df7fb8a0b2c1fd6eecf6fa1
Karahan <turkerkarahan24@gmail.com>
2023-07-26 12:29:20 +0300

    degisken isimleri güncellendi sag sol tespiti bu koda eklendi

--- python_arduino/el_kontrol.py
+++ python_arduino/el_kontrol.py
@@ -42,6 +42,19 @@ class HandGestureController:
                 for handLms in results.multi_hand_landmarks:
                     if self.previous_hand is None or handLms != self.previous_hand:
                         self.previous_hand = handLms
+
+                        x_coords = [lm.x * img.shape[1] for lm in handLms.landmark]
+                        y_coords = [lm.y * img.shape[0] for lm in handLms.landmark]
+                        min_x, max_x = int(min(x_coords)), int(max(x_coords))
+                        min_y, max_y = int(min(y_coords)), int(max(y_coords))
+
+                        # Elin tamamını kapsayacak şekilde sınırlayıcı kutu oluştur
+                        cv2.rectangle(img, (min_x - 20, min_y - 20), (max_x + 20, max_y + 20), (0, 255, 0), 2)
+
+                        # Elin yanına metin ekle
+                        hand_side = "Sag" if x_coords[17] > x_coords[5] else "Sol"
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        cv2.putText(img, hand_side + " el", (min_x - 70, min_y - 40), font, 1, (255, 255, 255), 2)
                         self.mpDraw.draw_landmarks(img, handLms, self.mpHand.HAND_CONNECTIONS)
 
                         x, y = handLms.landmark[3].x, handLms.landmark[3].y
@@ -62,26 +75,29 @@ class HandGestureController:
                         # Diğer el landmarkları için gerekli işlemleri burada yapabilirsiniz...
 
                         # Distance hesaplamaları ve açı dönüşümleri
-                        distance1 = int((x2 - x) * img.shape[0])
-                        distance2 = int((y2 - y3) * img.shape[0])
-                        distance3 = int((y4 - y5) * img.shape[0])
-                        distance4 = int((y6 - y7) * img.shape[0])
-                        distance5 = int((y8 - y9) * img.shape[0])
+                        sag_bas_parmak = int((x2 - x) * img.shape[0])
+                        sol_bas_parmak = int((x - x2) * img.shape[0])
+                        isaret_parmak = int((y2 - y3) * img.shape[0])
+                        orta_parmak = int((y4 - y5) * img.shape[0])
+                        yuzuk_parmak = int((y6 - y7) * img.shape[0])
+                        serce_parmak = int((y8 - y9) * img.shape[0])
 
-                        angle1 = int((distance1 / 70) * 170)
-                        angle2 = int((distance2 / 150) * 170)
-                        angle3 = int((distance3 / 175) * 170)
-                        angle4 = int((distance4 / 163) * 170)
-                        angle5 = int((distance5 / 133) * 170)
+                        sag_bas_parmak_angle = int((sag_bas_parmak / 70) * 170)
+                        sol_bas_parmak_angle = int((sol_bas_parmak / 70) * 170)
+                        isaret_parmak_angle = int((isaret_parmak / 150) * 170)
+                        orta_parmak_angle = int((orta_parmak / 175) * 170)
+                        yuzuk_parmak_angle = int((yuzuk_parmak / 163) * 170)
+                        serce_parmak_angle = int((serce_parmak / 133) * 170)
 
-                        angle1 = angle_limit(angle1, 0, 180)
-                        angle2 = angle_limit(angle2, 0, 180)
-                        angle3 = angle_limit(angle3, 0, 180)
-                        angle4 = angle_limit(angle4, 0, 180)
-                        angle5 = angle_limit(angle5, 0, 180)
+                        sag_bas_parmak_angle = angle_limit(sag_bas_parmak_angle, 0, 180)
+                        sol_bas_parmak_angle = angle_limit(sol_bas_parmak_angle, 0, 180)
+                        isaret_parmak_angle = angle_limit(isaret_parmak_angle, 0, 180)
+                        orta_parmak_angle = angle_limit(orta_parmak_angle, 0, 180)
+                        yuzuk_parmak_angle = angle_limit(yuzuk_parmak_angle, 0, 180)
+                        serce_parmak_angle = angle_limit(serce_parmak_angle, 0, 180)
 
-                        print("mesafe: " + str(distance2))
-                        print("aci: " + str(angle2))
+                        print("mesafe: " + str(isaret_parmak))
+                        print("aci: " + str(isaret_parmak_angle))
 
                         servo_num1 = 0
                         servo_num2 = 1
@@ -89,14 +105,19 @@ class HandGestureController:
                         servo_num4 = 3
                         servo_num5 = 4
 
-
-
-                        #if 180 > angle2 > 0:
-                        send_command(servo_num1, angle1)
-                        send_command(servo_num2, angle2)
-                        send_command(servo_num3, angle3)
-                        send_command(servo_num4, angle4)
-                        send_command(servo_num5, angle5)
+                        if hand_side == "Sag":
+                            send_command(servo_num1, sag_bas_parmak_angle)
+                            print("sag elllllllllllllllllllll")
+                        elif hand_side == "Sol":
+                            send_command(servo_num1, sol_bas_parmak_angle)
+                            print(hand_side)
+                        else:
+                            # Diğer durumlar için yapmak istediğiniz işlemi burada yapabilirsiniz.
+                            pass
+                        send_command(servo_num2, isaret_parmak_angle)
+                        send_command(servo_num3, orta_parmak_angle)
+                        send_command(servo_num4, yuzuk_parmak_angle)
+                        send_command(servo_num5, serce_parmak_angle)
 
 
 
