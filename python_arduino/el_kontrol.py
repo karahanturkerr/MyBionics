d582e7885ec10dc463d2dbfb3f3b637121f974a2
Karahan <turkerkarahan24@gmail.com>
2023-07-28 13:14:42 +0300

    kol_hareket.py ve el_kontrol.py asenkron calisabilmesi için düzenlendi, gereksiz kisimlar silindi

--- python_arduino/el_kontrol.py
+++ python_arduino/el_kontrol.py
@@ -1,142 +1,125 @@
 import cv2
 import time
 import mediapipe as mp
-import serial
-
-serial_port = 'COM5'
-baud_rate = 9600
-
-# Seri haberleşme başlat
-ser = serial.Serial(serial_port, baud_rate)
-time.sleep(2)  # Arduino'nun başlangıç için hazırlanması için kısa bir bekleme süresi
+import threading
 
 
-def send_command(servo_num, angle):
-    # Servo numarası ve açı değerini Arduino'ya gönder
-    command = f'{servo_num}:{angle}\n'
-    ser.write(command.encode())
-
-def angle_limit(value, min_value, max_value): #açıyı 0 ile 180 derece arasında sınırlandırır
+def angle_limit(value, min_value, max_value):  # açıyı 0 ile 180 derece arasında sınırlandırır
     return min(max(value, min_value), max_value)
 
 
 class HandGestureController:
-    def __init__(self, video_capture=0, serial_port='COM5', baud_rate=9600):
-        self.cap = cv2.VideoCapture(video_capture)
-        self.cap.set(3, 1280)
-        self.cap.set(4, 720)
+    def __init__(self, ser):
+        self.ser = ser
+
         self.mpHand = mp.solutions.hands
         self.hands = self.mpHand.Hands(max_num_hands=2)
         self.mpDraw = mp.solutions.drawing_utils
         self.previous_hand = None
+
         time.sleep(2)
 
-    def process_gestures(self):
-        while True:
-            success, img = self.cap.read()
-            img = cv2.flip(img, 1)
-            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            results = self.hands.process(imgRGB)
+        self.thread_lock = threading.Lock()
 
-            if results.multi_hand_landmarks:
-                for handLms in results.multi_hand_landmarks:
-                    if self.previous_hand is None or handLms != self.previous_hand:
-                        self.previous_hand = handLms
+    def send_command(self, servo_num, angle):
+        # Servo numarası ve açı değerini Arduino'ya gönder
 
-                        x_coords = [lm.x * img.shape[1] for lm in handLms.landmark]
-                        y_coords = [lm.y * img.shape[0] for lm in handLms.landmark]
-                        min_x, max_x = int(min(x_coords)), int(max(x_coords))
-                        min_y, max_y = int(min(y_coords)), int(max(y_coords))
+        with self.thread_lock:
+            command = f'{servo_num}:{angle}\n'
+            self.ser.write(command.encode())
 
-                        # Elin tamamını kapsayacak şekilde sınırlayıcı kutu oluştur
-                        cv2.rectangle(img, (min_x - 20, min_y - 20), (max_x + 20, max_y + 20), (0, 255, 0), 2)
+    def process_gestures(self, imgRGB, img):
+        results = self.hands.process(imgRGB)
 
-                        # Elin yanına metin ekle
-                        hand_side = "Sag" if x_coords[17] > x_coords[5] else "Sol"
-                        font = cv2.FONT_HERSHEY_SIMPLEX
-                        cv2.putText(img, hand_side + " el", (min_x - 70, min_y - 40), font, 1, (255, 255, 255), 2)
-                        self.mpDraw.draw_landmarks(img, handLms, self.mpHand.HAND_CONNECTIONS)
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                if self.previous_hand is None or handLms != self.previous_hand:
+                    self.previous_hand = handLms
 
+                    x_coords = [lm.x * img.shape[1] for lm in handLms.landmark]
+                    y_coords = [lm.y * img.shape[0] for lm in handLms.landmark]
+                    min_x, max_x = int(min(x_coords)), int(max(x_coords))
+                    min_y, max_y = int(min(y_coords)), int(max(y_coords))
 
-                        #elin noktalarının koordinatları hesaplanır
-                        x, y = handLms.landmark[3].x, handLms.landmark[3].y
-                        x1, y1 = handLms.landmark[4].x, handLms.landmark[4].y
+                    # Elin tamamını kapsayacak şekilde sınırlayıcı kutu oluştur
+                    cv2.rectangle(img, (min_x - 20, min_y - 20), (max_x + 20, max_y + 20), (0, 255, 0), 2)
 
-                        x2, y2 = handLms.landmark[5].x, handLms.landmark[5].y
-                        x3, y3 = handLms.landmark[8].x, handLms.landmark[8].y
+                    # Elin yanına metin ekle
+                    hand_side = "Sag" if x_coords[17] > x_coords[5] else "Sol"
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(img, hand_side + " el", (min_x - 70, min_y - 40), font, 1, (255, 255, 255), 2)
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHand.HAND_CONNECTIONS)
 
-                        x4, y4 = handLms.landmark[9].x, handLms.landmark[9].y
-                        x5, y5 = handLms.landmark[12].x, handLms.landmark[12].y
+                    # elin noktalarının koordinatları hesaplanır
+                    x, y = handLms.landmark[3].x, handLms.landmark[3].y
+                    x1, y1 = handLms.landmark[4].x, handLms.landmark[4].y
 
-                        x6, y6 = handLms.landmark[13].x, handLms.landmark[13].y
-                        x7, y7 = handLms.landmark[16].x, handLms.landmark[16].y
+                    x2, y2 = handLms.landmark[5].x, handLms.landmark[5].y
+                    x3, y3 = handLms.landmark[8].x, handLms.landmark[8].y
 
-                        x8, y8 = handLms.landmark[17].x, handLms.landmark[17].y
-                        x9, y9 = handLms.landmark[20].x, handLms.landmark[20].y
+                    x4, y4 = handLms.landmark[9].x, handLms.landmark[9].y
+                    x5, y5 = handLms.landmark[12].x, handLms.landmark[12].y
 
-                        # Diğer el landmarkları için gerekli işlemleri burada yapabilirsiniz...
+                    x6, y6 = handLms.landmark[13].x, handLms.landmark[13].y
+                    x7, y7 = handLms.landmark[16].x, handLms.landmark[16].y
 
-                        # Distance hesaplamaları ve açı dönüşümleri
-                        sag_bas_parmak = int((x2 - x) * img.shape[0])
-                        sol_bas_parmak = int((x - x2) * img.shape[0])
-                        isaret_parmak = int((y2 - y3) * img.shape[0])
-                        orta_parmak = int((y4 - y5) * img.shape[0])
-                        yuzuk_parmak = int((y6 - y7) * img.shape[0])
-                        serce_parmak = int((y8 - y9) * img.shape[0])
+                    x8, y8 = handLms.landmark[17].x, handLms.landmark[17].y
+                    x9, y9 = handLms.landmark[20].x, handLms.landmark[20].y
 
+                    # Diğer el landmarkları için gerekli işlemleri burada yapabilirsiniz...
 
-                        #aci ve limitleri belirlendi
-                        sag_bas_parmak_angle = angle_limit(int((sag_bas_parmak / 70) * 170), 0, 180)
-                        sol_bas_parmak_angle = angle_limit(int((sol_bas_parmak / 70) * 170), 0, 180)
-                        isaret_parmak_angle = angle_limit(int((isaret_parmak / 150) * 170), 0, 180)
-                        orta_parmak_angle = angle_limit(int((orta_parmak / 175) * 170), 0, 180)
-                        yuzuk_parmak_angle = angle_limit(int((yuzuk_parmak / 163) * 170), 0, 180)
-                        serce_parmak_angle = angle_limit(int((serce_parmak / 133) * 170), 0, 180)
+                    # Distance hesaplamaları ve açı dönüşümleri
+                    sag_bas_parmak = int((x2 - x) * img.shape[0])
+                    sol_bas_parmak = int((x - x2) * img.shape[0])
+                    isaret_parmak = int((y2 - y3) * img.shape[0])
+                    orta_parmak = int((y4 - y5) * img.shape[0])
+                    yuzuk_parmak = int((y6 - y7) * img.shape[0])
+                    serce_parmak = int((y8 - y9) * img.shape[0])
 
-                        print("mesafe: " + str(isaret_parmak))
-                        print("aci: " + str(isaret_parmak_angle))
+                    # aci ve limitleri belirlendi
+                    sag_bas_parmak_angle = angle_limit(int((sag_bas_parmak / 70) * 170), 0, 180)
+                    sol_bas_parmak_angle = angle_limit(int((sol_bas_parmak / 70) * 170), 0, 180)
+                    isaret_parmak_angle = angle_limit(int((isaret_parmak / 150) * 170), 0, 180)
+                    orta_parmak_angle = angle_limit(int((orta_parmak / 175) * 170), 0, 180)
+                    yuzuk_parmak_angle = angle_limit(int((yuzuk_parmak / 163) * 170), 0, 180)
+                    serce_parmak_angle = angle_limit(int((serce_parmak / 133) * 170), 0, 180)
 
-                        #servolar isimlerindirildi
-                        servo_sag_bp = 0
-                        servo_sag_ip = 1
-                        servo_sag_op = 2
-                        servo_sag_yp = 3
-                        servo_sag_sp = 4
+                    print("mesafe: " + str(isaret_parmak))
+                    print("aci: " + str(isaret_parmak_angle))
 
-                        servo_sol_bp = 0
-                        servo_sol_ip = 1
-                        servo_sol_op = 2
-                        servo_sol_yp = 3
-                        servo_sol_sp = 4
+                    # servolar isimlerindirildi
+                    servo_sag_bp = 0
+                    servo_sag_ip = 1
+                    servo_sag_op = 2
+                    servo_sag_yp = 3
+                    servo_sag_sp = 4
 
-                        #sag sol el ayrımı yapılır
-                        if hand_side == "Sag":
-                            send_command(servo_sag_bp, sag_bas_parmak_angle)
-                            print("sag elllllllllllllllllllll")
-                            send_command(servo_sag_ip, isaret_parmak_angle)
-                            send_command(servo_sag_op, orta_parmak_angle)
-                            send_command(servo_sag_yp, yuzuk_parmak_angle)
-                            send_command(servo_sag_sp, serce_parmak_angle)
+                    servo_sol_bp = 0
+                    servo_sol_ip = 1
+                    servo_sol_op = 2
+                    servo_sol_yp = 3
+                    servo_sol_sp = 4
 
-                        elif hand_side == "Sol":
-                            send_command(servo_sol_bp, sol_bas_parmak_angle)
-                            print(hand_side)
-                            send_command(servo_sol_ip, isaret_parmak_angle)
-                            send_command(servo_sol_op, orta_parmak_angle)
-                            send_command(servo_sol_yp, yuzuk_parmak_angle)
-                            send_command(servo_sol_sp, serce_parmak_angle)
-                        else:
-                            # Diğer durumlar için yapmak istediğiniz işlemi burada yapabilirsiniz.
-                            pass
+                    # sag sol el ayrımı yapılır
+                    if hand_side == "Sag":
+                        self.send_command(servo_sag_bp, sag_bas_parmak_angle)
+                        print("sag elllllllllllllllllllll")
+                        self.send_command(servo_sag_ip, isaret_parmak_angle)
+                        self.send_command(servo_sag_op, orta_parmak_angle)
+                        self.send_command(servo_sag_yp, yuzuk_parmak_angle)
+                        self.send_command(servo_sag_sp, serce_parmak_angle)
 
-
-            else:
-                self.previous_hand = None
-
-            cv2.imshow("img", img)
-            cv2.waitKey(1)
+                    elif hand_side == "Sol":
+                        self.send_command(servo_sol_bp, sol_bas_parmak_angle)
+                        print(hand_side)
+                        self.send_command(servo_sol_ip, isaret_parmak_angle)
+                        self.send_command(servo_sol_op, orta_parmak_angle)
+                        self.send_command(servo_sol_yp, yuzuk_parmak_angle)
+                        self.send_command(servo_sol_sp, serce_parmak_angle)
+                    else:
+                        # Diğer durumlar için yapmak istediğiniz işlemi burada yapabilirsiniz.
+                        pass
 
 
-# Sınıf örneğini oluşturup işlemi baslatırız
-controller = HandGestureController()
-controller.process_gestures()
+        else:
+            self.previous_hand = None
--- python_arduino/kol_hareket.py
+++ python_arduino/kol_hareket.py
@@ -1,99 +1,75 @@
+import threading
+
 import cv2
 import time
 import mediapipe as mp
-import serial
 import math
 
-class HandGestureController:
-    def __init__(self, serial_port='COM5', baud_rate=9600):
-        self.serial_port = serial_port
-        self.baud_rate = baud_rate
-        self.ser = serial.Serial(self.serial_port, self.baud_rate)
-        time.sleep(2)  # Arduino'nun başlangıç için hazırlanması için kısa bir bekleme süresi
 
-    def angle_limit(self, value, min_value, max_value): #açıyı 0 ile 180 derece arasında sınırlandırır
+class ArmGestureController:
+    def __init__(self, ser):
+        self.ser = ser
+
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose()
+        self.mpDraw = mp.solutions.drawing_utils
+
+        self.thread_lock = threading.Lock()
+
+    def angle_limit(self, value, min_value, max_value):  # açıyı 0 ile 180 derece arasında sınırlandırır
         return min(max(value, min_value), max_value)
 
     def send_command(self, servo_num, anglee):
         # Servo numarası ve açı değerini Arduino'ya gönder
-        command = f'{servo_num}:{anglee}\n'
-        self.ser.write(command.encode())
+
+        with self.thread_lock:
+            command = f'{servo_num}:{anglee}\n'
+            self.ser.write(command.encode())
 
     def findAngle(self, img, p1, p2, p3, lmList, draw=True):
         x1, y1 = lmList[p1][1:]
         x2, y2 = lmList[p2][1:]
         x3, y3 = lmList[p3][1:]
 
-        angle = math.degrees(math.atan2(y3-y2, x3-x2)-math.atan2(y1-y2, x1-x2)) #açı hesaplama
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))  # açı hesaplama
         if angle < 0: angle += 360
 
         angle = int(angle)
 
-        if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
-            cv2.line(img, (x3, y3), (x2, y2), (0, 0, 255), 3)
+        #
 
-            cv2.circle(img, (x1, y1), 10,  (0, 255, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y1), 10, (0, 255, 255), cv2.FILLED)
-            cv2.circle(img, (x3, y1), 10, (0, 255, 255), cv2.FILLED)
-
-            cv2.circle(img, (x1, y1), 15, (0, 255, 255))
-            cv2.circle(img, (x2, y2), 15, (0, 255, 255))
-            cv2.circle(img, (x3, y3), 15, (0, 255, 255))
-
-            #cv2.putText(img, str(int(angle)), (x2-40, y2+40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 2)
+        # cv2.putText(img, str(int(angle)), (x2-40, y2+40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 2)
         return angle
 
-    def process_gestures(self):
-        cap = cv2.VideoCapture(0)
-        cap.set(3, 640)
-        cap.set(4, 480)
+    def process_gestures(self, imgRGB, img):
+        results2 = self.pose.process(imgRGB)
 
-        mpPose = mp.solutions.pose
-        pose = mpPose.Pose()
-        mpDraw = mp.solutions.drawing_utils
+        lmList = []
+        if results2.pose_landmarks:
+            self.mpDraw.draw_landmarks(img, results2.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
 
-        while True:
-            success, img = cap.read()
-            img = cv2.flip(img, 1)
-            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            for id, lm in enumerate(results2.pose_landmarks.landmark):
+                h, w, _ = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
 
-            results2 = pose.process(imgRGB)
+            if len(lmList) != 0:
+                sag_dirsek = self.findAngle(img, 11, 13, 15, lmList)
+                sag_omuz = self.findAngle(img, 13, 11, 29, lmList)
+                sol_dirsek = 360 - self.findAngle(img, 12, 14, 22, lmList)
+                sol_omuz = 360 - self.findAngle(img, 14, 12, 24, lmList)
 
-            lmList = []
-            if results2.pose_landmarks:
-                mpDraw.draw_landmarks(img, results2.pose_landmarks, mpPose.POSE_CONNECTIONS)
+                # Açıları sınırlandırın
+                sag_dirsek = self.angle_limit(sag_dirsek, 0, 180)
+                sag_omuz = self.angle_limit(sag_omuz, 0, 180)
+                sol_dirsek = self.angle_limit(sol_dirsek, 0, 180)
+                sol_omuz = self.angle_limit(sol_omuz, 0, 180)
 
-                for id, lm in enumerate(results2.pose_landmarks.landmark):
-                    h, w, _ = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    lmList.append([id, cx, cy])
+                print(sol_omuz)
+                print("---------------------------------------")
+                print(sag_dirsek)
 
-                if len(lmList) != 0:
-                    sag_dirsek = self.findAngle(img, 11, 13, 15, lmList)
-                    sag_omuz = self.findAngle(img, 13, 11, 29, lmList)
-                    sol_dirsek = 360 - self.findAngle(img, 12, 14, 22, lmList)
-                    sol_omuz = 360 - self.findAngle(img, 14, 12, 24, lmList)
-
-
-                    # Açıları sınırlandırın
-                    sag_dirsek = self.angle_limit(sag_dirsek, 0, 180)
-                    sag_omuz = self.angle_limit(sag_omuz, 0, 180)
-                    sol_dirsek = self.angle_limit(sol_dirsek, 0, 180)
-                    sol_omuz = self.angle_limit(sol_omuz, 0, 180)
-
-                    print(sol_omuz)
-                    print("---------------------------------------")
-                    print(sag_dirsek)
-
-                    self.send_command(0, sag_dirsek)
-                    self.send_command(1, sag_omuz)
-                    self.send_command(2, sol_dirsek)
-                    self.send_command(3, sol_omuz)
-
-            cv2.imshow("image", img)
-            cv2.waitKey(1)
-
-if __name__ == "__main__":
-    controller = HandGestureController()
-    controller.process_gestures()
+                self.send_command(0, sag_dirsek)
+                self.send_command(1, sag_omuz)
+                self.send_command(2, sol_dirsek)
+                self.send_command(3, sol_omuz)
