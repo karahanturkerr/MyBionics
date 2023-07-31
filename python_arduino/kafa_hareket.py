24c2901d7fd5891e8d965f5d0af8f66a68fd7e27
Karahan <turkerkarahan24@gmail.com>
2023-07-31 16:11:29 +0300

    ortak olarak çalısması için  düzenlemeler yapıldı threading modülü entegre edildi

--- python_arduino/kafa_hareket.py
+++ python_arduino/kafa_hareket.py
@@ -1,95 +1,77 @@
 import cv2
 import mediapipe as mp
-import serial
 import time
+import threading
 
-#
-ser_port = 'COM5'
-baud_rate = 9600
 
-# Seri haberleşmeyi başlat
-ser = serial.Serial(port=ser_port, baudrate=baud_rate)
-time.sleep(2)
-
-def send_command(servo_num, angle):
-    # Servo numarası ve açı değerini Arduino'ya gönder
-    command = f'{servo_num}:{angle}\n'
-    ser.write(command.encode())
-
-def angle_limit(value, min_value, max_value): # Açıyı 0 ile 180 derece arasında sınırlandırır
-    return min(max(value, min_value), max_value)
 
 
 class HeadController:
-    def __init__(self, video_capture=0, serial_port='COM5', baud_rate=9600):
-        self.cap = cv2.VideoCapture(video_capture)
+    def __init__(self, ser):
+        self.ser = ser
         self.mpFaceDetection = mp.solutions.face_detection
         self.faceDetection = self.mpFaceDetection.FaceDetection()
         self.mpDraw = mp.solutions.drawing_utils
-        time.sleep(0.01)
+        self.thread_lock = threading.Lock()
 
-    def process_gestures(self):
-        while True:
-            success, img = self.cap.read()
-            img = cv2.flip(img, 1)
-            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            results = self.faceDetection.process(imgRGB)
 
-            if results.detections:
-                for id, detection in enumerate(results.detections):
-                    bboxC = detection.location_data.relative_bounding_box
-                    h, w, _ = img.shape
-                    bbox = int(bboxC.xmin * w), int(bboxC.ymin * h), int(bboxC.width * w), int(bboxC.height * h)
-                    cv2.rectangle(img, bbox, (0, 255, 255), 2)
+    def angle_limit(self, value, min_value, max_value):  # Açıyı 0 ile 180 derece arasında sınırlandırır
+        return min(max(value, min_value), max_value)
+    def send_command(self, servo_num, angle):
+        # Servo numarası ve açı değerini Arduino'ya gönder
+        with self.thread_lock:
+            command = f'{servo_num}:{angle}\n'
+            self.ser.write(command.encode())
 
-                    # Yüzün merkezini bul
-                    center_x = bbox[0] + bbox[2] // 2
-                    center_y = bbox[1] + bbox[3] // 2
+    def process_gestures(self, imgRGB, img):
 
-                    # Ekran genişliği ve yüksekliğini al
-                    screen_width, screen_height = w, h
+        results = self.faceDetection.process(imgRGB)
 
-                    # Yüz merkezinin ekran merkezine göre yatay ve dikey uzaklıklarını hesapla
-                    x_distance = center_x - screen_width // 2
-                    y_distance = center_y - screen_height // 2
+        if results.detections:
+            for id, detection in enumerate(results.detections):
+                bboxC = detection.location_data.relative_bounding_box
+                h, w, _ = img.shape
+                bbox = int(bboxC.xmin * w), int(bboxC.ymin * h), int(bboxC.width * w), int(bboxC.height * h)
+                cv2.rectangle(img, bbox, (0, 255, 255), 2)
 
-                    servo_kafa_ss = 1
-                    servo_kafa_ay = 2
+                # Yüzün merkezini bul
+                center_x = bbox[0] + bbox[2] // 2
+                center_y = bbox[1] + bbox[3] // 2
+
+                # Ekran genişliği ve yüksekliğini al
+                screen_width, screen_height = w, h
+
+                # Yüz merkezinin ekran merkezine göre yatay ve dikey uzaklıklarını hesapla
+                x_distance = center_x - screen_width // 2
+                y_distance = center_y - screen_height // 2
+
+                servo_kafa_ss = 0
+                servo_kafa_ay = 1
 
                     # Hareket yönünü belirle ve Arduino'ya gönder
-                    if abs(x_distance) > abs(y_distance):
-                        if x_distance > 50:
-                            movement = "Sag"
-                            send_command(servo_kafa_ss, angle_limit(90 + abs(x_distance), 0, 180))
-                            #print(x_distance)
-                        elif x_distance < -50:
-                            movement = "Sol"
-                            send_command(servo_kafa_ss, angle_limit(90 - abs(x_distance), 0, 180))
-                            #print(x_distance)
-                        else:
-                            movement = "Duz"
-                            send_command(servo_kafa_ss, 90)
-                            print(x_distance)
+                if abs(x_distance) > abs(y_distance):
+                    if x_distance > 50:
+                        movement = "Sag"
+                        self.send_command(servo_kafa_ss, self.angle_limit(90 + abs(x_distance), 40, 140))
+                        #print(x_distance)
+                    elif x_distance < -50:
+                        movement = "Sol"
+                        self.send_command(servo_kafa_ss, self.angle_limit(90 - abs(x_distance), 40, 140))
+                        #print(x_distance)
                     else:
-                        if y_distance > 70:
-                            movement = "Asagi"
-                            send_command(servo_kafa_ay, angle_limit(90 + abs(y_distance), 0, 180))
-                            print(y_distance)
-                        elif y_distance < -10:
-                            movement = "Yukari"
-                            send_command(servo_kafa_ay, angle_limit(90 - abs(y_distance), 0, 180))
-                        else:
-                            movement = "Duz"
-                            send_command(servo_kafa_ay, 90)
+                        movement = "Duz"
+                        self.send_command(servo_kafa_ss, 90)
+                        print(x_distance)
+                else:
+                    if y_distance > 70:
+                        movement = "Asagi"
+                        self.send_command(servo_kafa_ay, self.angle_limit(90 - abs(y_distance), 90, 75))
+                        print(y_distance)
+                    elif y_distance < -10:
+                        movement = "Yukari"
+                        self.send_command(servo_kafa_ay, self.angle_limit(90 + abs(y_distance), 90, 120))
+                    else:
+                        movement = "Duz"
+                        self.send_command(servo_kafa_ay, 90)
 
-                    cv2.putText(img, f"Hareket: {movement}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-            cv2.imshow("img", img)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-
-
-controller = HeadController()
-controller.process_gestures()
+                cv2.putText(img, f"Hareket: {movement}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
